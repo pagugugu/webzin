0ede1259afb71d5dbbb0d52f4eb39f7d64206504
pagugugu <132215961+pagugugu@users.noreply.github.com>
2023-08-04 04:19:02 +0900

    Add files via upload

---
+++ pollpage/ct_poll.sql
@@ -0,0 +1,17 @@
+use web03; 
+
+create table poll (
+     ans1 int(3),
+     ans2 int(3),
+     ans3 int(3),
+     ans4 int(4)
+);
+ 
+desc poll;
+
+create table polletc (
+     name VARCHAR(20),
+	 cont INT(3)
+);
+ 
+desc polletc;
